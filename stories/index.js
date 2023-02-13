21ecbfdb70df33e308ff78733c29778c42e4653e
Sabastian Omeasoo <sabomeasoo@gmail.com>
2023-02-12 17:32:59 -0800

    display appointments and schedule

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import "index.scss";
@@ -195,4 +195,20 @@ storiesOf("Appointment", module)
     onSave={action("onSave")} 
     interviewers={interviewers} 
     />
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
   ))
