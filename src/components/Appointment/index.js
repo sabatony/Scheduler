e6b21092d31e7fa2d97a4bd74e834ccf8e1692bf
Sabastian Omeasoo <sabomeasoo@gmail.com>
2023-02-22 16:56:30 -0800

    able to edit appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,6 +14,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
 
@@ -49,12 +50,12 @@ export default function Appointment(props) {
         time={props.time}
       />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={props.onEdit} onDelete={confirmDelete} />}
+      {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer} onEdit={() => transition(EDIT)} onDelete={confirmDelete} />}
       {mode === CREATE && <Form interviewers={props.interviewers} onSave={save} onCancel={() => back()} />}
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
       {mode === CONFIRM && <Confirm message="Are you sure you want to delete?" onConfirm={deleteAppointment} onCancel={() => back()}/> }
-
+      {mode === EDIT && <Form interviewer={props.interview.interviewer.id} student={props.interview.student} interviewers={props.interviewers} onCancel={() => back()} onSave={save}/>}
     </article>
   );
 }
