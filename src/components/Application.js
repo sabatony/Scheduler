0ff0b14aa9f4ce5e8328796f2bb65d12625fb507
Sabastian Omeasoo <sabomeasoo@gmail.com>
2023-02-24 17:43:25 -0800

    separation of concerns

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,8 @@ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "help
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index.js";
+import useApplicationData from "hooks/useApplicationData.js";
+
 
 // const appointments = {
 //   "1": {
@@ -46,76 +48,78 @@ import Appointment from "components/Appointment/index.js";
 
 
 export default function Application(props) {
+
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
   // const [day, setDay] = useState("monday");
   // const [days, setDays] = useState([]);
 
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
+  // const [state, setState] = useState({
+  //   day: 'Monday',
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {}
+  // });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  const setDay = day => setState({ ...state, day });
+  // const setDay = day => setState({ ...state, day });
   // const setDays = days => setState(prev => ({ ...prev, days }));
 
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then((all) => {
-      console.log(all);
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    });
-  }, []);
+  // useEffect(() => {
+  //   Promise.all([
+  //     axios.get("http://localhost:8001/api/days"),
+  //     axios.get("http://localhost:8001/api/appointments"),
+  //     axios.get("http://localhost:8001/api/interviewers")
+  //   ]).then((all) => {
+  //     console.log(all);
+  //     setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+  //   });
+  // }, []);
 
-  function bookInterview(id, interview) {
+  // function bookInterview(id, interview) {
 
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: { ...interview }
+  //   };
 
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
 
-    setState({
-      ...state,
-      appointments
-    });
+  //   setState({
+  //     ...state,
+  //     appointments
+  //   });
 
-    return axios
-    .put(`http://localhost:8001/api/appointments/${id}`, { interview })
-    .then(() => setState({...state, appointments}))
+  //   return axios
+  //   .put(`http://localhost:8001/api/appointments/${id}`, { interview })
+  //   .then(() => setState({...state, appointments}))
 
-  }
+  // }
 
-  function cancelInterview(id) {
+  // function cancelInterview(id) {
 
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
+  //   const appointment = {
+  //     ...state.appointments[id],
+  //     interview: null
+  //   };
 
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
+  //   const appointments = {
+  //     ...state.appointments,
+  //     [id]: appointment
+  //   };
 
-    // setState({
-    //   ...state,
-    //   appointments
-    // });
+  //   // setState({
+  //   //   ...state,
+  //   //   appointments
+  //   // });
 
-    return axios
-    .delete(`http://localhost:8001/api/appointments/${id}`)
-    .then(() => setState(prev => ({ ...prev, appointments }))); 
-   }
+  //   return axios
+  //   .delete(`http://localhost:8001/api/appointments/${id}`)
+  //   .then(() => setState(prev => ({ ...prev, appointments }))); 
+  //  }
 
   return (
     <main className="layout">
